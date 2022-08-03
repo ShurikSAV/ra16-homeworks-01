1415a924264234fe4ee5b967188069dbdaaa7d22
ShurikSAV <Shurik.SAV@Gmail.com>
2022-08-03 19:17:57 +0300

    добавил навигацию

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Header from './content/Header';
+import Shop from './content/Shop';
 
 function App() {
+  const menu = [
+    {
+      path: "/",
+      name: "Домой",
+      element: (
+        <p>Выберите задание в меню выше</p>
+      )
+    },
+    {
+      path: "/Shop",
+      name: "Магазин",
+      element: <Shop/>
+    }
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header menu={menu} />
+
+      <div className="App">
+        <Routes>
+          {/* <Route path="/Shop" element={<Shop/>}></Route> */}
+          {menu.map(
+            (item) => <Route {...item}></Route>
+          )}
+        </Routes>
+      </div>
     </div>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 export default App;
